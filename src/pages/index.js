128271fc0b29360d1609506a1e02fc3c25c7d00f
Alan del Fresno <81641419+AlandelFresno@users.noreply.github.com>
2023-05-22 16:06:32 -0300

    Revert "[Feauture] [dev] #5 Banner implementation"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,9 @@
 import React from "react";
 
-import { Button, Box, Typography, Grid} from "@mui/material";
-import Homepagebanner from "@/components/HomepageBanner";
-
-
-
-
-const Home = () => {
+export default function Home() {
   return (
-    <>
-
-    <Grid container spacing={0} sx={{ flexGrow: 2}}>
-  
-  <Grid item xs={12}>
-    
-    <Homepagebanner>
-
-    </Homepagebanner>
-            
-  </Grid>
-  
-  
-  
-  <Grid item xs={12}>
-      <Box bgcolor='red'> 
-        box2
-      </Box>
-  </Grid>
-
-  <Grid item xs={2}>   
-      <Box bgcolor='cyan'> 
-        box3
-      </Box>
-  </Grid>
-  
-  <Grid item xs={4}>
-      <Box bgcolor='green'> 
-        box3
-      </Box>
-  </Grid>
-
-</Grid>
-    <Box display="flex" justifyContent="center" alignContent="center">
-      <Typography variant="h2">Welcome to Focus</Typography>
-      
-    
-    </Box>
-    
-    </>
-    
+    <div>
+      <h2>hello world</h2>
+    </div>
   );
 }
-export default Home;
