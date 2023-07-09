3730ba7827bdbd6541825e5ebb3a420ed6d6c469
Alan del Fresno <alandelfresno2@gmail.com>
2023-07-08 23:18:48 -0300

    ScrollUp Button & Copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Box, Button, Icon, Typography } from '@mui/material';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <Box display="flex" flexDirection="Column">
+      <Button onClick={handleClick}>
+        <ArrowUpwardIcon />
+      </Button>
+      <Box display="flex" justifyContent="center">
+        <Typography variant="caption">Copyright 2023</Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
